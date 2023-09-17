c3e19781dd77a3ece3217c60191339dbb412d892
nownabe <nownabe@gmail.com>
2023-09-17 21:39:36 +0900

    test: add test for sourceLocation

---
+++ source_location_test.go
@@ -0,0 +1,31 @@
+package clog_test
+
+import (
+	"context"
+	"runtime"
+	"strconv"
+	"testing"
+
+	"github.com/nownabe/clog"
+)
+
+func Test_sourceLocation(t *testing.T) {
+	t.Parallel()
+
+	l, w := newLogger(clog.SeverityInfo)
+	ctx := context.Background()
+
+	pc, _, _, _ := runtime.Caller(0) // This must be called before calling l.Info
+	l.Info(ctx, "foo")
+
+	frame, _ := runtime.CallersFrames([]uintptr{pc}).Next()
+
+	want := buildWantLog("INFO", "foo")
+	want["logging.googleapis.com/sourceLocation"] = map[string]any{
+		"file":     frame.File,
+		"line":     strconv.Itoa(frame.Line + 1),
+		"function": frame.Function,
+	}
+
+	w.assertEqual(t, want)
+}
